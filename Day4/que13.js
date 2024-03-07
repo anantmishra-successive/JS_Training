99cf52a5ca1db9aea9d738528795dc6bf5175acb
anantmishra-successive <anant.mishra@successive.tech>
2024-03-07 10:05:49 +0530

    add:assignment4

--- Day4/que13.js
+++ Day4/que13.js
@@ -1,10 +1,23 @@
 //  Write a program to return inverse of an array
 
-const inverse = (arr) => {
-    arr.reverse(); 
-    console.log(arr);
-}
+const arr = [3, 4, 2, 0, 1];
+const inverseArr = new Array(arr.length);
 
+const makeInverseArray = (arr) => {
+  for (i in arr) {
+    inverseArr[arr[i]] = i;
+  }
+};
 
-const array = [2,3,4,5,6,7,8];
-inverse(array);
+const checkInverse = (arr, inverseArr) => {
+  for (i in arr) {
+    if (arr[i] != inverseArr[i]) {
+      console.log("The array inverse is not possible");
+      return false;
+    }
+  }
+  return true;
+};
+
+makeInverseArray(arr);
+console.log(inverseArr);
