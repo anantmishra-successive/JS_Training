03b8c8b6c4999dcb9b1358695d5b543948099201
anantmishra-successive <anant.mishra@successive.tech>
2024-03-14 16:13:33 +0530

    add:More methds

--- Day8/que9.js
+++ Day8/que9.js
@@ -18,3 +18,12 @@ class Person {
   var person = new Person("John Doe", 30);
   console.log(person.name);
   
+
+  // or
+
+  function Person3(name, age) {
+  this.name = name;
+  this.age = age;
+}
+var person = new Person3("John Doe", 30);
+console.log(person.name);
