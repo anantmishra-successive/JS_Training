6ec7b18969317a36aed227cd867feb116c92937d
anantmishra-successive <anant.mishra@successive.tech>
2024-03-18 10:52:15 +0530

    fix:issues

--- Day5/que5.js
+++ Day5/que5.js
@@ -19,5 +19,13 @@ const obj2 = {
       }
       return flattenedObj;
   }
+
+  const ansObj = flatten(obj2)
+  const result = Object.fromEntries(
+    Object.entries(ansObj).map((e) => [
+      [e[0].substring(1)], e[1]
+    ])
+  );
+  console.log(result);
   
-  console.log(flatten(obj2));
+//   console.log(flatten(obj2));
